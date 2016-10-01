28bcb81e809644c06f1b34f88b6e2404f9e0a833
Wouter van den Brink <wj.vandenbrink@gmail.com>
2016-10-01 14:03:01 +0200

    First composer project Commit

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wouter
+ * Date: 1-10-2016
+ * Time: 13:09
+ */
+
+namespace HelloWorld;
+
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer';
+    }
+
+}
